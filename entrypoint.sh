80d3872001fa15f18c3f59dfc3b24db39dfdb121
Marcel Donczyk <12114846+pmMarcel@users.noreply.github.com>
2022-04-06 18:40:30 +0200

    Fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -47,7 +47,7 @@ fi
 echo "The following image is going to be pushed to registry"
 echo $IMAGE
 
-docker build -t $IMAGE -f $DOCKERFILE $CONTEXT
+docker build -t $IMAGE -f $DOCKERFILE $CONTEXT_PATH
 docker login --username "$USERNAME" --password "$PASSWORD" $REGISTRY
 docker push $IMAGE
 
